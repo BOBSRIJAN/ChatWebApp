5d769b5c5c5c979161551fa48f1dc165fe61722e
SRIJAN_RAY <srijanray11@gmail.com>
2025-06-13 18:54:06 +0530

    deploy error fix

--- msgapp/msgapp/settings.py
+++ msgapp/msgapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^c(@e!slg=&z$%#u6d(wz$e19vr)hm!se@=z(sryqx@$a8)gbq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://chatwebapp-14d9.onrender.com', '*']
 
 
 # Application definition
